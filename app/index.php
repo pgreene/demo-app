40a11b4294b6d0f2e84cfb2a9e6df42745f2a53b
pgreene <pgreene@protonmail.com>
2025-02-26 11:56:38 -0500

    Added deploy step to pipeline

--- app/index.php
+++ app/index.php
@@ -8,7 +8,7 @@ $conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die("Connection failed ... sad. : " . $conn->connect_error);
 }
 echo "Connected to RDS successfully";
 ?>
