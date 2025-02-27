5b9e73adeee31bea0b240c9823c93c700c2ee35c
pgreene <pgreene@protonmail.com>
2025-02-27 12:54:00 -0500

    updated select query

--- app/view.php
+++ app/view.php
@@ -1,12 +1,12 @@
 <?php include 'connect.php';?>
 <?php
-$sql = "SELECT id, firstname, lastname FROM clients";
+$sql = "SELECT firstname, lastname, email FROM clients ORDER BY lastname";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "Name: " . $row["firstname"]. " " . $row["lastname"]. "email: " . $row["email"]. "<br>";
+    echo "Name: " . $row["firstname"]. " " . $row["lastname"]. " | email: " . $row["email"]. "<br>";
   }
 } else {
   echo "0 results";
