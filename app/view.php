89aa69396752576a7d5a6dae7405ae9428b9fa7c
pgreene <pgreene@protonmail.com>
2025-02-27 13:18:10 -0500

    updated sql for record retrieval

--- app/view.php
+++ app/view.php
@@ -1,12 +1,11 @@
 <?php include 'connect.php';?>
 <?php
-$sql = "SELECT firstname, lastname, email FROM clients ORDER BY lastname";
+$sql = "SELECT DISTINCT ON (firstname, lastname) firstname, lastname, email FROM clients WHERE firstname IS NOT NULL OR lastname IS NOT NULL ORDER BY lastname";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    #echo "<b>NAME:</b> " . $row["firstname"]. " " . $row["lastname"]. " | <b>E-MAIL:</b> " . $row["email"]. "<br>";
     echo $row["firstname"]. " " . $row["lastname"]. "<br>";
 
   }
