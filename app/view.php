a4ff6a212f5a281c3cee8e62635e79be4289f529
pgreene <pgreene@protonmail.com>
2025-02-27 12:58:50 -0500

    Formatting output

--- app/view.php
+++ app/view.php
@@ -6,7 +6,10 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "<b>NAME:</b> " . $row["firstname"]. " " . $row["lastname"]. " | <b>E-MAIL:</b> " . $row["email"]. "<br>";
+    #echo "<b>NAME:</b> " . $row["firstname"]. " " . $row["lastname"]. " | <b>E-MAIL:</b> " . $row["email"]. "<br>";
+    echo "<b>NAMES</b> <br>" ;
+    echo $row["firstname"]. " " . $row["lastname"]. "<br>";
+
   }
 } else {
   echo "0 results";
