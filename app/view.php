9d9230227d018fd59cfb6a09585c3622d7346aa9
pgreene <pgreene@protonmail.com>
2025-02-27 12:50:33 -0500

    updated how records are displayed

--- app/view.php
+++ app/view.php
@@ -6,7 +6,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "Name: " . $row["firstname"]. " " . $row["lastname"]. "email: " . $row["email"]. "<br>";
   }
 } else {
   echo "0 results";
