b270265d46667bbf4ea82618f7f5cecba1caebc9
pgreene <pgreene@protonmail.com>
2025-02-27 13:09:09 -0500

    last update

--- app/view.php
+++ app/view.php
@@ -7,7 +7,7 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     #echo "<b>NAME:</b> " . $row["firstname"]. " " . $row["lastname"]. " | <b>E-MAIL:</b> " . $row["email"]. "<br>";
-    echo "<b>NAMES</b> " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo $row["firstname"]. " " . $row["lastname"]. "<br>";
 
   }
 } else {
